119179cc5ae38dd18239fc8282e19fc63982f461
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-09 14:33:07 +0100

    fixed bug in AppDbContext class

--- UrlShortener.DAL/Data/AppDbContext.cs
+++ UrlShortener.DAL/Data/AppDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UrlShortener.DAL.Entities;
 
 namespace UrlShortener.DAL.Data
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions <AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<UrlManagement> Urls { get; set; }
     }
 }

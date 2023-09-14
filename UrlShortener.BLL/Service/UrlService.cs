715be987dfff74b0fcf9ef3599b6cc557a1161df
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-14 20:34:08 +0100

    Added a service class to refactor code

---
+++ UrlShortener.BLL/Service/UrlService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.BLL.Interfaces;
+
+namespace UrlShortener.BLL.Service
+{
+    public class UrlService : IUrlService
+    {
+        public string GetOriginalUrl(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShortenUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

7171b4af8bf7c3f4516f56a1752f174eaab8afa0
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-15 09:46:00 +0100

    Added interface

---
+++ UrlShortener.BLL/Interfaces/IUrlService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.BLL.Interfaces
+{
+    public interface IUrlService
+    {
+        string ShortenUrl(string url);
+        string GetOriginalUrl(string shortUrl);
+
+    }
+}

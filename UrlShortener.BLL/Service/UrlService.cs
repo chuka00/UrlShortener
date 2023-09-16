f1e4efa02b498fe4852534c842b5ea460dff760e
Chuka Ogbodo <chukaogbodo7@gmail.com>
2023-09-16 21:47:01 +0100

    Added some logic to service class. still has some bugs

--- UrlShortener.BLL/Service/UrlService.cs
+++ UrlShortener.BLL/Service/UrlService.cs
@@ -4,19 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UrlShortener.BLL.Interfaces;
+using UrlShortener.DAL.Data;
+using UrlShortener.DAL.Entities;
 
 namespace UrlShortener.BLL.Service
 {
     public class UrlService : IUrlService
     {
-        public string GetOriginalUrl(string shortUrl)
+        private readonly AppDbContext _dbContext;
+        public UrlService(AppDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
         }
 
         public string ShortenUrl(string url)
         {
-            throw new NotImplementedException();
+            if (!Uri.TryCreate(url, UriKind.Absolute, out var inputUrl))
+                return null; // Invalid URL
+
+            // Check if the URL already exists in the database
+            var existingUrl = _dbContext.Urls.FirstOrDefault(u => u.Url == url);
+            if (existingUrl != null)
+            {
+               // return $"{yourBaseUrl}/{existingUrl.ShortUrl}"; // Return existing short URL
+            }
+
+            // Generate a random short URL
+            var random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#";
+            var randomStr = new string(Enumerable.Repeat(chars, 8)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+            var sUrl = new UrlManagement()
+            {
+                Url = url,
+                ShortUrl = randomStr
+            };
+
+            _dbContext.Urls.Add(sUrl);
+            _dbContext.SaveChanges();
+
+            return sUrl.ShortUrl;
+
+           // return $"{yourBaseUrl}/{sUrl.ShortUrl}"; // Return newly generated short URL
         }
+
+        public string GetOriginalUrl(string shortUrl)
+        {
+            var urlMatch = _dbContext.Urls.FirstOrDefault(x => x.ShortUrl == shortUrl);
+
+            if (urlMatch == null)
+                return null; // Short URL not found in the database
+
+            return urlMatch.Url; // Return the original URL
+        }
+
     }
 }
